2918325ca2655e4913083124024809389b204b64
ttt <ttt@ALE-554RL5K>
2017-07-11 17:12:42 -0400

    Modifify program

--- COMP-123-SUMMER-Lesson9B/Program.cs
+++ COMP-123-SUMMER-Lesson9B/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*Name: Alessandro Santos
+ * Data:2017-07-11
+ * Version 0.1 - Creatd the program class */
+
 namespace COMP_123_SUMMER_Lesson9B
 {
     class Program
